670d617ad09bbdafa61da86c26ff8317980bd627
BonazziMirco <mirco.bonazzi@studenti.unitn.it>
2026-07-05 19:30:51 +0200

    bug fix

--- models/index.js
+++ models/index.js
@@ -5,16 +5,11 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// Use the application's configured sequelize instance so models that initialize
+// themselves (via Device.init(...) etc.) share the same connection.
+const sequelize = require(__dirname + '/../config/database');
 
 fs
   .readdirSync(__dirname)
@@ -27,8 +22,28 @@ fs
     );
   })
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
+    const imported = require(path.join(__dirname, file));
+
+    // Some model files export a function (sequelize-cli style):
+    //   module.exports = (sequelize, DataTypes) => { ... }
+    // Others export a Model class that has already been initialized against
+    // the shared `sequelize` instance and therefore should be used directly.
+    let model;
+    if (typeof imported === 'function' && imported.prototype && imported.prototype.constructor.name === 'Function') {
+      // If it's a factory function, call it with our sequelize and DataTypes
+      try {
+        model = imported(sequelize, Sequelize.DataTypes);
+      } catch (e) {
+        // If calling fails (for example the import is a class), fall back to using the import directly
+        model = imported;
+      }
+    } else {
+      model = imported;
+    }
+
+    if (model && model.name) {
+      db[model.name] = model;
+    }
   });
 
 Object.keys(db).forEach(modelName => {
