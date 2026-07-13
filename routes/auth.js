4352423b8152dc833c252f4e098c41089fb2a9d6
BonazziMirco <mirco.bonazzi@studenti.unitn.it>
2026-07-13 20:45:46 +0200

    changes to check session

--- routes/auth.js
+++ routes/auth.js
@@ -6,8 +6,8 @@ const authorityMiddleware = require('../middleware/authority_middleware');
 router.post('/register', authController.registerUser);
 router.post('/login', authController.loginUser);
 router.post('/logout', authController.logoutUser);
-router.post('/refresh', authorityMiddleware.requireAnyUser, authController.refreshUser);
-router.head('/checkSession', authorityMiddleware.requireAnyUser, authController.checkSession)
+router.post('/refresh', authController.refreshUser);
+router.get('/checkSession', authorityMiddleware.requireAnyUser, authController.checkSession)
 
 router.post('/device/register', authorityMiddleware.requireAdmin, authController.registerDevice)
 router.post('/device/login', authController.loginDevice)
